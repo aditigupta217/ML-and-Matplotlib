f572fd9fc4d20880735862aef3d307b3a391fa61
aditigupta217 <aditigupta221177@gmail.com>
2025-08-12 22:49:03 +0530

    a

--- chp2.py
+++ chp2.py
@@ -1,3 +1,22 @@
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 import pandas as pd
 
+# Sample dataset
+data = {
+    'studyhours': [1, 2, 3, 4, 5],
+    'marks': [60, 65, 70, 75, 80]
+}
+
+df = pd.DataFrame(data)
+
+# Standard Scaler
+standard_scaler = StandardScaler()
+standard_scaled = standard_scaler.fit_transform(df)
+print("Standard Scaler Output:")
+print(pd.DataFrame(standard_scaled, columns=['studyhours', 'marks']))
+
+# Min-Max Scaler
+minmax_scaler = MinMaxScaler()
+minmax_scaled = minmax_scaler.fit_transform(df)
+print("\nMin-Max Scaler Output:")
+print(pd.DataFrame(minmax_scaled, columns=['studyhours', 'marks']))
